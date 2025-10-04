18d2888df0d48496423d48083b04cad21911c176
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-04 19:55:14 +0530

    Create 004_important_string(method1).py

---
+++ 004_important_string(method1).py
@@ -0,0 +1,19 @@
+
+
+# strings in Python
+
+
+name = str(input("Enter Your name : "))
+
+age = int(input("Enter Your Age : "))
+
+
+# print("My name is %s and I am of %d ." %(name,age))
+
+
+# print("My name is {} and I am {} year's old." .format(name,age))
+
+
+
+
+
