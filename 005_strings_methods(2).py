543d22a2f853d9cf034f910d446e128bf25ce6bb
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-04 21:45:26 +0530

    Create 005_strings_methods(2).py

---
+++ 005_strings_methods(2).py
@@ -0,0 +1,42 @@
+# string slicing
+
+
+name = "Shaurya Tiwari"
+
+name2 = "Divyanshu Tiwari"
+
+name1 = name[0:8]
+
+print(name1[::-1])
+
+
+
+name3 = name2.replace("Tiwari","Tripathi")
+
+[firstname,lastname] = name.split(' ')
+
+# print(name3)
+
+print(firstname)
+print(lastname)
+
+
+
+# String Methods
+# UPPERCASE
+print(name.upper())
+
+# LOWERCASE
+print(name.lower())
+
+print(name.count('a'))
+
+
+print(name.find('S'))
+
+
+
+
+
+
+
