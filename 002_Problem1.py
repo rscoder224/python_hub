1e55d27731b97c2f0fc41ba2fe9dbdcfe73710dd
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-04 19:22:40 +0530

    Create 002_Problem1.py

---
+++ 002_Problem1.py
@@ -0,0 +1,16 @@
+
+# Problem 1
+
+num = int(input("Enter a number : "))
+
+
+if num >= 0 :
+    if(num > 0 and num % 2 == 0):
+        print("number is Positive and Even")
+    else:
+        print("Number is Positive But not Even")
+else:
+    print("Number is Negative")
+    
+    
+    
