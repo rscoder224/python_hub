676a3cda5e7590756f5c7c39b112feae30d3c417
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-06 20:47:10 +0530

    Create 008_lists_Methods.py

---
+++ 008_lists_Methods.py
@@ -0,0 +1,51 @@
+# lists in Python
+
+animal = ["lion","bear","deer","elephant","Hippo"]
+
+print(animal)
+
+
+
+# print(animal[::-1])
+
+# count = 0;
+
+# while count < len(animal):
+#       print(animal[count])
+#       count += 1
+      
+
+# list methods in pyhton
+# append
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+# arr.append("kallu")
+
+print(arr)
+
+# extend
+
+arr2 = [23,22,1,2,449,202]
+
+
+arr2.extend(arr)
+
+# 
+
+arr2.sort()
+
+
+print(arr2)
+
+
+
+
+
+
+
+
+
+
+
+
+
