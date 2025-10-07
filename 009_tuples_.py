831410cae4aee2501422d442d9e7aad15e748b7e
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-07 22:23:59 +0530

    Create 009_tuples_.py

---
+++ 009_tuples_.py
@@ -0,0 +1,35 @@
+
+# tuples
+# duplicates are allowed
+# in tuples traversing is allowed
+# hetrogenius
+# index based access
+# you can't resassing any value in touple once it is declared
+
+a = (1,2,3,4,5,6,78,888)
+
+print(a[4])
+
+# for i in a:
+#     print(i)
+    
+    
+for i in range(0,len(a),1):
+          print(a[i])
+         
+         
+print(a.index(888))
+
+print(a.count(888))
+
+a,b,c,d = (1,2,3,4)
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+
+
