c84bbfd3d9fac217eb473033550fca8fc2e2d292
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-10-09 22:55:11 +0530

    Create 010_sets_in_python.py

---
+++ 010_sets_in_python.py
@@ -0,0 +1,27 @@
+
+# sets in Python
+a = {12,474,373,2837,"hello"}
+# no order is followed 
+# set methods
+
+# for i in a:
+#     print(a)
+    
+
+
+a.remove(12)
+
+a.add("Jaggu")
+
+
+print(a)
+
+
+b = {3,474,373}
+
+s = a.union(b)
+
+print(s);
+
+
+
